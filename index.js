6aa33dd55cae01264e7c8c460a558c7d5b0ecd06
Rafael GS <rgs1990419@gmail.com>
2020-01-30 12:14:25 -0300

    Query & Route Params

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World!" });
+// Query params = ?teste=1
+server.get("/query", (req, res) => {
+  const nome = req.query.nome;
+
+  return res.json({ message: `Hello ${nome}!` });
+});
+
+// Route params = /route/1
+server.get("/route/:id", (req, res) => {
+  const { id } = req.params;
+  // const id = req.params.id;
+
+  return res.json({ message: `Buscando id: ${id}, através de Route params` });
 });
 
 server.listen(3000);
